5bdea881f1110df1e3ef8dbc2a556741d7faf2eb
AndreaCatapano <a.catapano96@gmail.com>
2025-04-10 14:35:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//Hook
+import { useState, useEffect } from 'react'
+
+//Data 
+import moviesData from './data/movies.js'
+//Css
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
