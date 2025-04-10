3bc74eb86444e760ee0020ff8055200e7f3de422
AndreaCatapano <a.catapano96@gmail.com>
2025-04-10 15:40:13 +0200

    feat: created seatchtitle

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,33 @@ function App() {
 
   const [movies, setMovies] = useState(moviesData);
   const [searchedGenre, setSearchedGenre] = useState('');
+  const [searchTitle, setSearchTitle] = useState('')
 
 
 
   useEffect(() => {
+    console.log(searchTitle)
     let result = moviesData;
     if (searchedGenre !== '') {
-      console.log(searchedGenre)
       result = moviesData.filter(movie => movie.genre === searchedGenre);
     }
 
     setMovies(result);
-  }, [searchedGenre])
+  }, [searchedGenre, searchTitle])
 
   return (
     <>
-      <select value={searchedGenre} onChange={e => setSearchedGenre(e.target.value)}>
-        <option value="">--</option>
-        <option value="Fantascienza">Fantascienza</option>
-        <option value="Thriller">Thriller</option>
-        <option value="Romantico">Romantico</option>
-        <option value="Azione">Azione</option>
-      </select>
+      <div>
+        <select value={searchedGenre} onChange={e => setSearchedGenre(e.target.value)}>
+          <option value="">--</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
 
+        <input type="text" value={searchTitle} onChange={e => setSearchTitle(e.target.value)} />
+      </div>
       <ul>
         {movies.map((movie, index) => (
           <li key={index}>{movie.title}</li>
