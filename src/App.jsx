43960adf6f682ef769f5a689b4bed95af01f3486
AndreaCatapano <a.catapano96@gmail.com>
2025-04-10 15:27:59 +0200

    feat: added select for genre

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,22 @@ import './App.css'
 function App() {
 
   const [movies, setMovies] = useState(moviesData);
+  const [searchedGenre, setSearchedGenre] = useState('');
+
+
+
+  useEffect(() => { console.log(searchedGenre) }, [searchedGenre])
 
   return (
     <>
+      <select value={searchedGenre} onChange={e => setSearchedGenre(e.target.value)}>
+        <option value="">--</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+      </select>
+
       <ul>
         {movies.map((movie, index) => (
           <li key={index}>{movie.title}</li>
